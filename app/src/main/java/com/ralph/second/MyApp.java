fa8205df2cdda9d277929d6c08b11988c580aa36
guozhanxian <guozhanxian@gmail.com>
2016-06-22 15:15:02 +0800

    ralph

--- app/src/main/java/com/ralph/second/MyApp.java
+++ app/src/main/java/com/ralph/second/MyApp.java
@@ -2,6 +2,8 @@ package com.ralph.second;
 
 import android.app.Application;
 
+import com.baidu.android.pushservice.PushConstants;
+import com.baidu.android.pushservice.PushManager;
 import com.baidu.mapapi.SDKInitializer;
 
 /**
@@ -11,6 +13,8 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         SDKInitializer.initialize(getApplicationContext());
+        PushManager.startWork(getApplicationContext(), PushConstants.LOGIN_TYPE_API_KEY,"TPr2fI9C9YCBQvnPZQhqpDXw");
+        PushManager.enableLbs(getApplicationContext());
         super.onCreate();
     }
 }
